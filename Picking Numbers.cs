f3bdfcaa7d26d16be66c2fa26974fc1be7f54f8a
Ailyn <aiasanchez1989@gmail.com>
2025-10-03 17:02:54 +0800

    Picking Numbers.cs

---
+++ Picking Numbers.cs
@@ -0,0 +1,63 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<int> a)
+    {
+         var frequency = new int[101];
+        foreach (var number in a)
+        {
+            frequency[number]++;
+        }
+
+        int maxCount = 0;
+        for (int i = 1; i < frequency.Length; i++)
+        {
+            maxCount = Math.Max(maxCount, frequency[i] + frequency[i - 1]);
+        }
+
+        return maxCount;
+    }
+
+    }
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.pickingNumbers(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
